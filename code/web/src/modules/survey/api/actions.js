de8d32824905e654229c49251281cc90aa5a85a3
Nick Hart <nick.hart.des@gmail.com>
2020-10-11 16:42:52 -0600

    Add example action types for survey

---
+++ code/web/src/modules/survey/api/actions.js
@@ -0,0 +1,15 @@
+// formatted similar to other actions files in this code base, this will contain
+// necessary network requests and action creators as well as dispatch as needed
+
+// similar imports for network requests, subject to change
+import axios from "axios";
+import { query, mutation } from "gql-query-builder";
+
+// example action types
+export const GET_MENS_CLOTHING = ''
+export const GET_WOMENS_CLOTHING = ''
+export const GET_MENS_ACCESSORIES = ''
+export const GET_WOMENS_ACCESSORIES = ''
+export const GET_ALL_MENS = ''
+export const GET_ALL_WOMENS = ''
+
