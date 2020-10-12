7049a7f4b4747c103dc0ab31f6bfa7ff7f7b51b3
Nick Hart <nick.hart.des@gmail.com>
2020-10-11 18:55:15 -0600

    Finish route annotation in App

--- code/web/src/setup/client/App.js
+++ code/web/src/setup/client/App.js
@@ -14,13 +14,17 @@ const App = () => (
   <Layout>
     {/* Set up the switch wrapper, so only one exact path can be accessed at a time */}
     <Switch>
-
+      {/* iterating over all routes passed from setup/routes/index.js */}
       {Object.values(routes).map((route, index) => (
+        // check if route.auth is truthy...
         route.auth
-          ? <RoutePrivate {...route} key={index} path={typeof route.path === 'function' ? route.path() : route.path}/>
-          : <Route {...route} key={index} path={typeof route.path === 'function' ? route.path() : route.path}/>
+          ? /* if it is, use the object to set up a RoutePrivate */ 
+          <RoutePrivate {...route} key={index} path={typeof route.path === 'function' ? route.path() : route.path}/>
+          : /* if it's not, make a normal route using the info from the route object */ 
+          <Route {...route} key={index} path={typeof route.path === 'function' ? route.path() : route.path}/>
       ))}
 
+      {/* default route for 404 page/wrong path */}
       <Route component={NotFound}/>
     </Switch>
   </Layout>
