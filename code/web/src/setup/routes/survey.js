c20ebaafbe535c73fd48ad6025e5a3c334d6ac81
Nick Hart <nick.hart.des@gmail.com>
2020-10-11 18:43:20 -0600

    Outline survey path file

---
+++ code/web/src/setup/routes/survey.js
@@ -0,0 +1,13 @@
+// Following codebase convention, importing the component to be exported with
+// the path object
+
+import Page from '../../modules/survey/Page';
+
+// Survey routes (following convention from other path files)
+
+export default {
+  survey: {
+    path: 'style/preferences',
+    component: Page
+  }
+}
