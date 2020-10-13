8b5105090063544dc4554159f1af0e9fbf51b793
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-10-12 21:29:41 -0400

    Create Style model and add one-to-many relationship with Product

---
+++ code/api/src/modules/style/model.js
@@ -0,0 +1,16 @@
+'use strict'
+
+module.exports = function(sequelize, DataTypes) {
+  let Style = sequelize.define('styles', {
+    name: {
+      type: DataTypes.STRING
+    }
+  })
+
+  Style.associate = function(models) {
+    Style.hasMany(models.User)
+    Style.hasMany(models.Product)
+  }
+
+  return Style
+}
