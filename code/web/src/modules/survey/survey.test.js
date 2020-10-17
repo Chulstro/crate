22aff08bf2f5daf830b1f3b0d98c36263160df42
Amy Karnaze <akarnaze@gmail.com>
2020-10-17 16:43:45 -0600

    Refactor state and add survey reset and clothing selection test

--- code/web/src/modules/survey/survey.test.js
+++ code/web/src/modules/survey/survey.test.js
@@ -3,7 +3,7 @@ import { surveyInfo } from './api/state'
 describe('Survey Info', () => {
   it('should have a move forward case', () => {
     const fakeState = {
-        views: ['survey-start', 'tops', 'bottoms', 'accessories', 'shoes', 'survey-finish'],
+        views: ['survey-start'],
         currentView: 0,
         crateId: null,
         isLoading: false,
@@ -33,7 +33,7 @@ describe('Survey Info', () => {
 
   it('should reset style', () => {
     const fakeState = {
-        views: ['survey-start', 'tops', 'bottoms', 'accessories', 'shoes', 'survey-finish'],
+        views: ['survey-start'],
         currentView: 2,
         crateId: null,
         isLoading: false,
@@ -44,5 +44,37 @@ describe('Survey Info', () => {
     const result = surveyInfo(fakeState, {type: 'SURVEY/RESET_SURVEY'})
 
     expect(result.currentView).toEqual(0);
+  })
+
+  it('should reset style', () => {
+    const fakeState = {
+      views: ['survey-start'],
+      currentView: 0,
+      crateId: null,
+      isLoading: false,
+      error: null,
+      clothingList: [],
+      selectedClothing: {},
+      userStyle: ""
+      }
+
+    const result = surveyInfo(fakeState, {type: 'SURVEY/SUBMIT_SURVEY'})
+    expect(result.currentView).toEqual(0);
+  })
+
+ it('should select clothing', () => {
+    const fakeState = {
+      views: ['survey-start'],
+      currentView: 0,
+      crateId: null,
+      isLoading: false,
+      error: null,
+      clothingList: [],
+      selectedClothing: {},
+      userStyle: ""
+      }
+
+    const result = surveyInfo(fakeState, {type: 'SURVEY/SELECT_CLOTHING'})
+    expect(result.selectedClothing).toMatchObject({ selectedClothing: 'bottoms' });
   })
 })
