076a993f0917548599f22f7f3d5cbca5a0c62cca
Nick Hart <nick.hart.des@gmail.com>
2020-10-17 15:50:23 -0600

    Alter seeder for uniform categories

--- code/api/src/seeders/5-surveyProducts.js
+++ code/api/src/seeders/5-surveyProducts.js
@@ -33,28 +33,28 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        category: 'tops',
+        category: 'top',
         image: '/images/stock/punk-top-female.jpg',
         styleId: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'tops',
+        category: 'top',
         image: '/images/stock/punk-top-male.jpg',
         styleId: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'bottoms',
+        category: 'bottom',
         image: '/images/stock/punk-pants.jpg',
         styleId: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'bottoms',
+        category: 'bottom',
         image: '/images/stock/punk-pants.jpg',
         styleId: 1,
         createdAt: new Date(),
@@ -62,42 +62,42 @@ module.exports = {
       },
       //Sporty
       {
-        category: 'sunglasses',
+        category: 'sunglasse',
         image: '/images/stock/sporty-sunglasses.jpg',
         styleId: 2,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'sunglasses',
+        category: 'sunglasse',
         image: '/images/stock/sporty-sunglasses.jpg',
         styleId: 2,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'bottoms',
+        category: 'bottom',
         image: '/images/stock/sporty-pants-male.jpg',
         styleId: 2,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'bottoms',
+        category: 'bottom',
         image: '/images/stock/sporty-pants-female.jpg',
         styleId: 2,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'tops',
+        category: 'top',
         image: '/images/stock/sporty-top-male.jpg',
         styleId: 2,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'tops',
+        category: 'top',
         image: '/images/stock/sporty-top-female.jpg',
         styleId: 2,
         createdAt: new Date(),
@@ -105,42 +105,42 @@ module.exports = {
       },
       //Casual
       {
-        category: 'watches',
+        category: 'watch',
         image: '/images/stock/casual_watch.jpg',
         styleId: 3,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'watches',
+        category: 'watch',
         image: '/images/stock/casual_watch.jpg',
         styleId: 3,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'tops',
+        category: 'top',
         image: '/images/stock/casual_top_female.jpg',
         styleId: 3,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'tops',
+        category: 'top',
         image: '/images/stock/casual_top_male.jpg',
         styleId: 3,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'bottoms',
+        category: 'bottom',
         image: '/images/stock/casual_pants_female.jpeg',
         styleId: 3,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'bottoms',
+        category: 'bottom',
         image: '/images/stock/casual_pants_male.jpg',
         styleId: 3,
         createdAt: new Date(),
@@ -150,7 +150,7 @@ module.exports = {
 
       //Given
       {
-        category: 'belts',
+        category: 'belt',
         image: '/images/stock/belt-female.jpg',
         styleId: 4,
 
@@ -158,7 +158,7 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        category: 'belts',
+        category: 'belt',
         image: '/images/stock/belt-male.jpg',
         styleId: 4,
 
@@ -166,7 +166,7 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        category: 'watches',
+        category: 'watch',
         image: '/images/stock/watch-female.jpg',
         styleId: 4,
 
@@ -174,7 +174,7 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        category: 'watches',
+        category: 'watch',
         image: '/images/stock/watch-male.jpg',
         styleId: 4,
 
@@ -182,7 +182,7 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        category: 'tops',
+        category: 'top',
         image: '/images/stock/t-shirt-female-1.jpg',
         styleId: 4,
 
@@ -190,21 +190,21 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        category: 'tops',
+        category: 'top',
         image: '/images/stock/t-shirt-female-2.jpg',
         styleId: 4,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'tops',
+        category: 'top',
         image: '/images/stock/t-shirt-male-1.jpg',
         styleId: 4,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'tops',
+        category: 'top',
         image: '/images/stock/t-shirt-male-2.jpg',
         styleId: 4,
         createdAt: new Date(),
@@ -213,56 +213,56 @@ module.exports = {
 
       //Vintage
       {
-        category: 'belts',
+        category: 'belt',
         image: '/images/stock/belt-female.jpg',
         styleId: 5,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'belts',
+        category: 'belt',
         image: '/images/stock/belt-male.jpg',
         styleId: 5,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'watches',
+        category: 'watch',
         image: '/images/stock/watch-female.jpg',
         styleId: 5,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'watches',
+        category: 'watch',
         image: '/images/stock/watch-male.jpg',
         styleId: 5,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'tops',
+        category: 'top',
         image: '/images/stock/t-shirt-female-1.jpg',
         styleId: 5,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'tops',
+        category: 'top',
         image: '/images/stock/t-shirt-female-2.jpg',
         styleId: 5,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'tops',
+        category: 'top',
         image: '/images/stock/t-shirt-male-1.jpg',
         styleId: 5,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        category: 'tops',
+        category: 'top',
         image: '/images/stock/t-shirt-male-2.jpg',
         styleId: 5,
         createdAt: new Date(),
